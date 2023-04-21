271659e48833ef3a4ebf42442ac156049b09efae
AbdurrahmanVarol <96303254+AbdurrahmanVarol@users.noreply.github.com>
2023-04-21 10:38:35 +0300

    Proje yapısı düzenlendi. MVC projesi eklendi.

---
+++ VotingApp/VotingAppçMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VotingAppçMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
