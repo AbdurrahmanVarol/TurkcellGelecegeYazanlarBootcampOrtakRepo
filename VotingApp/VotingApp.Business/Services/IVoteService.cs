9fbfed864ac010d04c152e6e3a3a488cec214198
AbdurrahmanVarol <96303254+AbdurrahmanVarol@users.noreply.github.com>
2023-04-28 18:30:29 +0300

    Vote servis class'ı ve interface'i oluşturuldu

---
+++ VotingApp/VotingApp.Business/Services/IVoteService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VotingApp.Business.Requests;
+using VotingApp.Business.Responses;
+
+namespace VotingApp.Business.Services
+{
+    public interface IVoteService
+    {
+        Task<List<VoteResponse>> GetVotesAsync();
+        Task<List<VoteResponse>> GetVotesByUserIdAsync(int userId);
+        Task<VoteResponse> GetVotesByIdAsync(int id);
+        Task<VoteReportResponse> GetVoteReportById(int id);
+        Task<VoteResponse> AddAsync(CreateVoteRequest createVoteRequest);
+        Task<VoteResponse> UpdateAsync(ModifyVoteRequest modifyVoteRequest);
+        Task<VoteResponse> DeleteAsync(int id);
+    }
+}
