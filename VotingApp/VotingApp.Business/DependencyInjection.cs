3d707f3a0c12197f443fe955e029389f13ab7849
wiaoj <bertandeniz7@gmail.com>
2023-04-28 20:27:44 +0300

    added data access layer dependency injection

---
+++ VotingApp/VotingApp.Business/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using VotingApp.DataAccess;
+
+namespace VotingApp.Business;
+public static class DependencyInjection {
+    public static IServiceCollection AddBusinessLayer(this IServiceCollection services, IConfiguration configuration) {
+
+        services.AddDataAccessLayer(configuration);
+        return services;
+    }
+}
