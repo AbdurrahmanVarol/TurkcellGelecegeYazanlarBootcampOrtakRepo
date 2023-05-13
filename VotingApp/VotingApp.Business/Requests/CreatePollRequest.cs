1698ebe14e352a51b821414bc099dadb6ecda650
Yalçın Selçuk <yalcinselcukkk@outlook.com>
2023-05-13 15:35:13 +0300

    Abdurrahman ile Create Poll oluşturuldu

--- VotingApp/VotingApp.Business/Requests/CreatePollRequest.cs
+++ VotingApp/VotingApp.Business/Requests/CreatePollRequest.cs
@@ -4,7 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VotingApp.Business.Requests {
-    public class CreatePollRequest {
+namespace VotingApp.Business.Requests
+{
+    public class CreatePollRequest
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public List<string> OptionNames { get; set; }
     }
 }
