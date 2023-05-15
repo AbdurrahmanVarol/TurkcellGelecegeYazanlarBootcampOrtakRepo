7da7f8dd186216ae050ac38ce106344d29faee36
AbdurrahmanVarol <96303254+AbdurrahmanVarol@users.noreply.github.com>
2023-05-15 21:05:22 +0300

    alertifyjs eklendi ve createpoll sayfası düzenlendi

--- VotingApp/VotingApp.MVC/Controllers/PollController.cs
+++ VotingApp/VotingApp.MVC/Controllers/PollController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using VotingApp.Business.Requests;
 using VotingApp.Business.Services;
 
@@ -7,7 +8,7 @@ namespace VotingApp.MVC.Controllers
     public class PollController : Controller
     {
         private readonly IPollService _pollService;
-
+        private int UserId => int.Parse(User.Claims.Where(x => x.Type == ClaimTypes.NameIdentifier).First().Value);
         public PollController(IPollService pollService)
         {
             _pollService = pollService;
@@ -22,6 +23,7 @@ namespace VotingApp.MVC.Controllers
         [HttpPost]
         public IActionResult CreatePoll([FromBody] CreatePollRequest request)
         {
+            request.CreatedById = UserId;
             var result = _pollService.CreatePollAsync(request).GetAwaiter().GetResult();
             return Json(new { isSuccess = result });
         }
